ba3a0c5595640ba9f3fa4fd12573c6cbfd27d745
Sniperghost662009 <125476080+Sniperghost662009@users.noreply.github.com>
2023-09-24 01:00:15 -0600

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
   try {
     const pp = imagen4;
     // let vn = './src/pictures/Sin Ttítulo.png'
-    const img = '../Menu2.jpg';
+    const img = './src/pictures/Sin Ttítulo.png';
     const d = new Date(new Date + 3600000);
     const locale = 'es';
     const week = d.toLocaleDateString(locale, {weekday: 'long'});
