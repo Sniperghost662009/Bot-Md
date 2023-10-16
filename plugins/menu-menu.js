947c63a59fe6b79966cb01dbb67d14ca8f894068
Sniperghost662009 <125476080+Sniperghost662009@users.noreply.github.com>
2023-10-15 21:41:09 -0600

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
   try {
-    const pp = /Menu.png;
+    const pp = imagen4;
     const week = d.toLocaleDateString(locale, {weekday: 'long'});
     const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
     const _uptime = process.uptime() * 1000;
