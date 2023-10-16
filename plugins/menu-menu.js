22b40ad87cce7cfcec4a61bb7837472f4902cbfe
Sniperghost662009 <125476080+Sniperghost662009@users.noreply.github.com>
2023-10-15 20:51:39 -0600

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
-  try {  
-    src ="https://i.ibb.co/H7MkMjc/giphy.webp" alt="giphy" border="0" /></a>
-    const pp = <a href="https://imgbb.com/"><img src="https://i.ibb.co/H7MkMjc/giphy.webp" alt="giphy" border="0" /></a>;
-    const a href="https://imgbb.com/"><img src="https://i.ibb.co/H7MkMjc/giphy.webp" alt="giphy" border="0" /></a>;
+  try {
+    const pp = imagen4;
+    // let vn = './media/menu.mp3'
+    const img = './Menu2.jpg';
     const d = new Date(new Date + 3600000);
     const locale = 'es';
     const week = d.toLocaleDateString(locale, {weekday: 'long'});
